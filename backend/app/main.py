dbad9798820c2ca897d6f2711906577c6eb0b996
black <black@system.com>
2025-09-27 21:25:21 +0200

    add connection to postgresdb

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,13 @@
 from typing import Union
-
 from fastapi import FastAPI
+from sqlmodel import Field, SQLModel, create_engine
 
 app = FastAPI()
+
+postgres_url = "postgresql://changeme:changeme@db:5432/db"
+engine = create_engine(postgres_url, echo=True)
+
+SQLModel.metadata.create_all(engine)
 
 
 @app.get("/")

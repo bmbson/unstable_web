e446248f46d69902529fedce77a654a6bbf4495f
pc <pc@example.com>
2025-11-06 23:23:09 +0100

    working on lifting the state

--- frontend/app/ust_dig/src/app/page.tsx
+++ frontend/app/ust_dig/src/app/page.tsx
@@ -3,15 +3,49 @@
 import Image from 'next/image'
 import './page.css';
 import SlideDisplay from '@/components/slide_display/slide_display';
+import CarouselIndicator from '@/components/carousel_indicator/carousel_indicator';
+import { useEffect, useState } from 'react';
 
 const string = '/images/frontpage_images/sea.jpg'
 
 export default function Home() {
+	const [index, setIndex] = useState<number>(1);
+
+	function nextItem() {
+		if (index - 1 >= index + 1) {
+			setIndex(index + 1);
+		} else {
+			setIndex(0);
+		}
+	}
+
+	function prevItem() {
+		if (index <= 0) {
+			setIndex(index - 1);
+		} else {
+			setIndex(index - 1);
+		}
+	}
+
+	useEffect(() => {
+		const scroll = setInterval(() => {
+			nextItem();
+		}, 5000);
+
+		return () => {
+			clearInterval(scroll);
+		};
+	},); // optional dependency array
+
+
 	return (
 		<main>
 			<div className="frontPageWrapper">
 				<div className="ImageWrapper">
-					<SlideDisplay src={string} alt="test"></SlideDisplay>
+					<SlideDisplay src={string} alt="test" index={index}></SlideDisplay>
+				</div>
+				<div className='CarouselWrapper'>
+					<CarouselIndicator index={3} ></CarouselIndicator>
 				</div>
 			</div>
 		</main >
--- frontend/app/ust_dig/src/components/carousel_indicator/carousel_indicator.tsx
+++ frontend/app/ust_dig/src/components/carousel_indicator/carousel_indicator.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 import './carousel_indicator.css';
 
-function CarouselIndicator(): any {
+function CarouselIndicator(num: number) {
+
+	function createElements() {
+		let elements = [];
+		for (let i = 0; i < num; i++) {
+			elements.push(
+				<div key={num} className="dot">x</div>
+			);
+		}
+		return elements;
+	}
+
+
 	return (
 		<div>
-			<div id="carouselIndicator"></div>
+			<div id="carouselIndicator">
+				{
+					createElements()
+				}
+
+			</div>
 		</div>
 	);
 };

a318d7bc50c84b5e24d7da2cffc9bb99a39535db
pc <pc@example.com>
2025-11-06 16:33:27 +0100

    work on carousel

---
+++ frontend/app/ust_dig/src/components/carousel_indicator/carousel_indicator.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './carousel_indicator.css';
+
+function CarouselIndicator(): any {
+	return (
+		<div>
+			<div id="carouselIndicator"></div>
+		</div>
+	);
+};
+
+export default CarouselIndicator

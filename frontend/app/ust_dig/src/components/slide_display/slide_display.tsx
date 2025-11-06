2abbccacd7f7ae20d41dcf836d65bdb381ce4dad
pc <pc@example.com>
2025-11-06 23:23:09 +0100

    working on lifting the state

--- frontend/app/ust_dig/src/components/slide_display/slide_display.tsx
+++ frontend/app/ust_dig/src/components/slide_display/slide_display.tsx
@@ -27,42 +27,15 @@ const items: slideDisplayImageItem[] = [
 	},
 ];
 
-function SlideDisplay({ src, alt, }: { src: string; alt: string; }) {
-	const [index, setIndex] = useState(0);
-
-	function nextItem() {
-		if (items.length - 1 >= index + 1) {
-			setIndex(index + 1);
-		} else {
-			setIndex(0);
-		}
-	}
-
-	function prevItem() {
-		if (index <= 0) {
-			setIndex(items.length - 1);
-		} else {
-			setIndex(index - 1);
-		}
-	}
-
-	// clear de interval later ook wanneer je op nextItem() of prevItem() klikt.
-	useEffect(() => {
-		const scroll = setInterval(() => {
-			nextItem();
-		}, 5000)
-		return () => {
-			clearInterval(scroll)
-		}
-	})
+function SlideDisplay({ src, alt, index }: { src: string; alt: string; index: any }) {
 
 	return (
 		<div className="slideDisplayWrapper">
-			<div className="slideDisplayControl" id="slideDisplayControl-L" onClick={() => { prevItem() }} ></div>
+			<div className="slideDisplayControl" id="slideDisplayControl-L" onClick={() => { }} ></div>
 			<Link id="linkWrapper" href={items[index].url}>
 				<Image className="image" fill={true} src={items[index].imageUrl} alt={items[index].title} ></Image>
 			</Link>
-			<div className="slideDisplayControl" id="slideDisplayControl-R" onClick={() => { nextItem() }} ></div>
+			<div className="slideDisplayControl" id="slideDisplayControl-R" onClick={() => { }} ></div>
 		</div >
 	)
 }

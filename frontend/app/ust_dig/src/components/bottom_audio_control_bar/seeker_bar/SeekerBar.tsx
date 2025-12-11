a95e132056cd30bd798e8fd11213142c96d768b9
pc <pc@example.com>
2025-12-11 23:08:34 +0100

    fix audio pausing when audiolength has been reached.

--- frontend/app/ust_dig/src/components/bottom_audio_control_bar/seeker_bar/SeekerBar.tsx
+++ frontend/app/ust_dig/src/components/bottom_audio_control_bar/seeker_bar/SeekerBar.tsx
@@ -1,9 +1,10 @@
 'use client'
 
 import './SeekerBar.css';
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { fancyTimeFormat, reverseFancyTimeFormat } from '@/app/ts/timeManipulationHelpers';
 import { useAudioContextHelperStore } from '@/store';
+import { ctx } from '@/app/audioContextBackendClass';
 
 // TODO: 
 // Seekerbar is still choppy.
@@ -19,16 +20,40 @@ function SeekerBar(props: Props) {
 	const inputRef = useRef(null);
 
 	const changeCurrentTime = useAudioContextHelperStore((state) => state.changeCurrentTime);
+	const setCurrentTime = useAudioContextHelperStore((state) => state.setCurrentTime);
+	const setAudioLength = useAudioContextHelperStore((state) => state.setAudioLength);
+	const currentElement = useAudioContextHelperStore((state) => state.currentElement);
+	const currentTime = useAudioContextHelperStore((state => state.currentTime));
+	const audioLength = useAudioContextHelperStore((state => state.audioLength));
+	const setIsAudioPlaying = useAudioContextHelperStore((state => state.setIsAudioPlaying));
+
 
 	function seekerBarEffect() {
 		changeCurrentTime(inputRef.current.value);
 	}
 
+	useEffect(() => {
+		setInterval(() => {
+			// Update currentAudioTime every 10ms.
+			setAudioLength();
+			setCurrentTime();
+
+			if (ctx.audioElement!.currentTime >= ctx.audioElement!.duration) {
+				setIsAudioPlaying(false); // Update React state
+				ctx.pauseAudioElement();  // Pause actual audio
+			}
+		}, 50);
+
+	}, [currentTime]);
+
 	return (<div id="seekerBar">
 		<p id="currentAudioTime">{props.currentTime}</p>
-		<input ref={inputRef} id="seekerBarSlider" type="range" min="0" onChange={() => seekerBarEffect()} value={reverseFancyTimeFormat(props.currentTime)!} max={reverseFancyTimeFormat(props.audioDuration)!} step="any"></input>
+		<input ref={inputRef} id="seekerBarSlider" type="range" min="0" onChange={() => {
+			seekerBarEffect()
+		}
+		} value={reverseFancyTimeFormat(props.currentTime)!} max={reverseFancyTimeFormat(props.audioDuration)!} step="any"></input>
 		<p id="totalAudioDuration">{props.audioDuration}</p>
-	</div>)
+	</div >)
 }
 
 export default SeekerBar;

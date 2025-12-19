6ae794f6ab9ff5600f3acc624d94a03a0d5f2cb5
pc <pc@example.com>
2025-12-19 02:34:14 +0100

    working on visual page

--- frontend/app/ust_dig/src/app/visuals/page.tsx
+++ frontend/app/ust_dig/src/app/visuals/page.tsx
@@ -1,16 +1,28 @@
-import CablesPatch from "@/components/cable_patch/cables_patch"
+'use client'
 import React from "react"
 import './page.css'
+import { P5Canvas, Sketch } from "@p5-wrapper/react"
+
+const sketch: Sketch = p5 => {
+	p5.setup = () => p5.createCanvas(600, 400, p5.WEBGL);
+
+	p5.draw = () => {
+		p5.background(250);
+		p5.normalMaterial();
+		p5.push();
+		p5.rotateZ(p5.frameCount * 0.01);
+		p5.rotateX(p5.frameCount * 0.01);
+		p5.rotateY(p5.frameCount * 0.01);
+		p5.plane(100);
+		p5.pop();
+	};
+};
 
 export default function Visuals() {
 	return (
 		<main>
 			<div className="textDiv">
-				<p>Work</p>
-				<p>In</p>
-				<p>Process</p>
-				{/* <CablesPatch/>
-        <CablesPatch patchDir={"/patch1/"} canvasId={"othercanvas"} patchOptions={{ "glCanvasResizeToWindow": false }}/> */}
+				<P5Canvas sketch={sketch} />
 			</div>
 		</main>
 	)
